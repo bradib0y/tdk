c3226f2469e80c479352992dabc9a645e8dba393
Balazs Pukli <balazsptm@gmail.com>
2018-07-27 02:22:14 +0200

    github

---
+++ github.bat
@@ -0,0 +1,6 @@
+echo "# AjanlatPavaPatika" >> README.md
+git init
+git add README.md
+git commit -ma "github"
+git remote add origin https://github.com/bradib0y/AjanlatPavaPatika.git
+git push -u origin master
